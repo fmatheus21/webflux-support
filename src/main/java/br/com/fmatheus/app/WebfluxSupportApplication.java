32415f4dcbc8fc4000126b30883ba38577a7c688
Fernando Matheus <fernando.matheuss@hotmail.com>
2025-03-03 18:59:14 -0300

    first commit

---
+++ src/main/java/br/com/fmatheus/app/WebfluxSupportApplication.java
@@ -0,0 +1,13 @@
+package br.com.fmatheus.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxSupportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxSupportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fmatheus/app/WebfluxSupportApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fmatheus.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxSupportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
